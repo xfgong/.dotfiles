9f05cbcc6a9d46dedaad36da786e6284eca6cf55
Chris <gongxiangfeng02@meituan.com>
2013-11-13 10:32:05 +0800

    update python checker

--- python_checker.sh
+++ python_checker.sh
@@ -4,28 +4,21 @@ if [ $? -ne 0 ]; then
     echo "         Try pip install flake8"
 fi
 
+alias pychecker="flake8 --show-pep8 --show-source --ignore=W291,W391,W402,W404,W601,E123,E124,E125,E126,E127,E128,E226,E261,E262,E501,E502,E711,E712"
+
 function check() {
+    #check python code in a git repo
     exit=0
     for file in `git diff --cached --name-only --diff-filter=ACMRTUXB HEAD|grep '\.py$'`; do
-        flake8 --ignore=W291,W391,W402,W404,W601,E123,E124,E125,E126,E127,E128,E226,E261,E262,E501,E502,E711,E712 $file
+        pychecker $file
         if [ $? -ne 0 ]; then
             let exit=1
         fi
     done
 
     if [ "$0" != "check" ];then
+        #userd in other scripts
         exit $exit
-    fi
-}
-
-function check_dir () 
-{
-    if [[ "$#" -lt 1 ]]; then
-        echo "Check Python Script Syntax Before Submitting to the Repo!!!";
-        echo "Usage: $FUNCNAME <project_path_or_python_filename>";
-        return;
-    else
-        flake8 --ignore=W291,W391,W402,W404,W601,E123,E124,E125,E126,E127,E128,E226,E261,E262,E501,E502,E711,E712 "$1"
     fi
 }
 
